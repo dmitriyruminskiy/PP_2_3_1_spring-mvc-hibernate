c74b7a8d42c393980099198c405b3ff16bc0a3c6
Dima Rumynskii <ra-z88@mail.ru>
2023-03-06 14:04:52 +0300

    Second commit

---
+++ src/main/java/web/service/UsersServiceImpl.java
@@ -0,0 +1,49 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.entities.User;
+
+import java.util.List;
+@Repository
+public class UsersServiceImpl implements UsersService{
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UsersServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User show(int id) {
+        return userDao.show(id);
+    }
+
+    @Override
+    @Transactional()
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(User updateUser) {
+        userDao.update(updateUser);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+}
